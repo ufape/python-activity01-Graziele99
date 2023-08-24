0d6bc1e93f6d5d138f42c10ea6081ec43d24f84c
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-24 16:04:16 -0300

    Update problem_06.py

--- problem_06.py
+++ problem_06.py
@@ -28,37 +28,7 @@ O maior número é: 106
 
 
 def main():
-    # -*- coding: utf-8 -*-
-
-# YOUR FULL NAME
-# UAG00098
-# Problem Set 1 - Problem 6
-# Description:
-
-
-"""
-Inputs, Processes and Output (IPO)
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-Input(s):
-O arquivo de entrada contém três valores inteiros.
-7 14 106
-
-Processes:
-Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
-
-maiorAB = (a + b + abs(a - b)) / 2
-
-Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
-
-Output(s):
-Imprima conforme exemplo a seguir.
-Exemplo:
-O maior número é: 106
-"""
-
-
-def main():
-    a = int(input("Digite o valor A: "))        
+      a = int(input("Digite o valor A: "))        
     b = int(input("Digite o valor B: "))
     c = int(input("Digite o valor C: "))
     
@@ -66,11 +36,6 @@ def main():
     maiorABC = (maiorAB + c + abs(maiorAB - c)) // 2
   
     print(f"O maior número é: {maiorABC}")
-
-
-if __name__ == '__main__':
-    main()
-
 
 
 if __name__ == '__main__':
