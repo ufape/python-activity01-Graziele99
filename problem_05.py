57cc827d29344267f8ac5913bc85bc21f8ac02b2
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-24 16:02:05 -0300

    Update problem_05.py

--- problem_05.py
+++ problem_05.py
@@ -26,43 +26,13 @@ Exemplo:
 Volume: 14137.1550L
 """
 
-
-def main():
-    # -*- coding: utf-8 -*-
-
-# YOUR FULL NAME
-# UAG00098
-# Problem Set 1 - Problem 5
-# Description:
-
-
-"""
-Inputs, Processes and Output (IPO)
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-Input(s):
-A entrada contém um valor de ponto flutuante, no caso, a variável raio.
-Exemplo:
-Digite o valor do raio: 15.00
-
-
-Processes:
-A fórmula para calcular o volume é: (4/3) · π · R³. Considere o
-valor de π igual a 3.14159.
-
-Output(s):
-A saída deverá ser uma mensagem "Volume: " conforme o exemplo fornecido abaixo.
-O valor deverá ser apresentado com 4 casas após o ponto.
-Exemplo:
-Volume: 14137.1550L
-"""
-
-
+  
 def main():
     PI = 3.14159
     raio = float(input("Digite o valor do raio: "))
     volume = (4/3) * PI * (raio ** 3)
     print(f"Volume: {volume:.4f}L")
-  
+    
   
 
 
