8c4bc4a2aa7991cc19d4dcb31d308f2141b77a37
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-24 12:41:39 -0300

    Update problem_04.py

--- problem_04.py
+++ problem_04.py
@@ -30,7 +30,49 @@ Exemplo:
 
 
 def main():
-    pass # your code here
+    # -*- coding: utf-8 -*-
+
+# YOUR FULL NAME
+# UAG00098
+# Problem Set 1 - Problem 4
+# Description:
+
+
+"""
+Inputs, Processes and Output (IPO)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+Input(s):
+A entrada contém um valor de ponto flutuante, no caso, a variável raio.
+Exemplo:
+Digite o valor do raio: 2.00
+
+
+Processes:
+A fórmula para calcular a área de uma circunferência é: area = π・raio².
+Considere π = 3.14159:
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e
+multiplicando por π.
+
+Output(s):
+Apresentar a mensagem "Área = " seguido pelo valor da variável area, conforme
+exemplo abaixo, com 5 casas após o ponto decimal.
+Exemplo:
+Área = 12.56636m
+"""
+
+
+def main():
+    PI = 3.14159
+    raio = float(input( " Digite o valor de raio: "))
+  
+    área = PI * (raio**2)
+
+    print(f"Área: {área:.5f}m") 
+
+
+if __name__ == '__main__':
+    main()
+
 
 if __name__ == '__main__':
     main()
