465d817c6032f3e553431fcc6371cac6a5773b1d
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-24 12:37:48 -0300

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -28,7 +28,45 @@ Soma = 19
 
 
 def main():
-    pass # your code here
+    # -*- coding: utf-8 -*-
+
+# YOUR FULL NAME
+# UAG00098
+# Problem Set 1 - Problem 2
+# Description:
+
+"""
+Inputs, Processes and Output (IPO)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+Input(s):
+A entrada contém 2 valores inteiros, conforme a seguir:
+Digite o valor de A: 10
+Digite o valor de B: 9
+
+Processes:
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de
+A e B atribuindo o seu resultado na variável soma. Imprima soma conforme
+exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo
+que está sendo especificado.
+
+Output(s):
+Imprima a mensagem "Soma = " (letra S maiúscula) seguido pelo valor da
+variável soma. Cuide para que tenha um espaço antes e depois do sinal
+de igualdade, conforme o exemplo abaixo.
+Soma = 19
+"""
+
+
+def main():
+   a = int(input("Digite o valor de A: "))
+   b = int(input("Digite o valor de B: "))
+   soma = a + b
+   print(f"Soma = {soma}")
+  
+    
+if __name__ == '__main__':
+    main()
+
 
 
 if __name__ == '__main__':
