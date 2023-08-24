8b3c5f971fb675ce31492a6c6f337fe0d207c4e6
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-24 15:03:30 -0300

    atividade2

--- problem_02.py
+++ problem_02.py
@@ -28,88 +28,11 @@ Soma = 19
 
 
 def main():
-    # -*- coding: utf-8 -*-
-
-# YOUR FULL NAME
-# UAG00098
-# Problem Set 1 - Problem 2
-# Description:
-
-"""
-Inputs, Processes and Output (IPO)
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-Input(s):
-A entrada contém 2 valores inteiros, conforme a seguir:
-Digite o valor de A: 10
-Digite o valor de B: 9
-
-Processes:
-Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de
-A e B atribuindo o seu resultado na variável soma. Imprima soma conforme
-exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo
-que está sendo especificado.
-
-Output(s):
-Imprima a mensagem "Soma = " (letra S maiúscula) seguido pelo valor da
-variável soma. Cuide para que tenha um espaço antes e depois do sinal
-de igualdade, conforme o exemplo abaixo.
-Soma = 19
-"""
-
-
-def main():
-   
-  # -*- coding: utf-8 -*-
-
-# YOUR FULL NAME
-# UAG00098
-# Problem Set 1 - Problem 2
-# Description:
-
-"""
-Inputs, Processes and Output (IPO)
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-Input(s):
-A entrada contém 2 valores inteiros, conforme a seguir:
-Digite o valor de A: 10
-Digite o valor de B: 9
-
-Processes:
-Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de
-A e B atribuindo o seu resultado na variável soma. Imprima soma conforme
-exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo
-que está sendo especificado.
-
-Output(s):
-Imprima a mensagem "Soma = " (letra S maiúscula) seguido pelo valor da
-variável soma. Cuide para que tenha um espaço antes e depois do sinal
-de igualdade, conforme o exemplo abaixo.
-Soma = 19
-"""
-
-
-def main():
-   
-  print("Programa Simples de Pagamento\n")
-    horas_trabalhadas = int(input("Informe quantas horas você trabalhou: "))
-    print("\nContracheque")
-    print(f"\t\tHoras trabalhadas: {horas_trabalhadas}")
-    valor_da_hora_trabalhada = 25.00
-    print(f"\t\tValor da Hora: R${valor_da_hora_trabalhada:.2f}")
-    imposto = 0.125
-    salario_bruto = valor_da_hora_trabalhada * horas_trabalhadas 
-    print(f"\t\tSalário Bruto: R${salario_bruto:.2f}")
-    impostos_a_cobrar =  salario_bruto * imposto 
-    print(f"\t\tImposto: R${impostos_a_cobrar:.2f}")
-    salario_liquido = salario_bruto - impostos_a_cobrar 
-    print(f"\t\tSalário Líquido: R${salario_liquido:.2f}")
+    a = int(input('Digite o valor de A: '))
+    b = int(input('Digite o valor de B: '))
+    soma = a + b
+    print(f'Soma = {soma}')
     
-if __name__ == '__main__':
-    main()
-
-    
-if __name__ == '__main__':
-    main()
 
 
 
